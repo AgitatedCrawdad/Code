a316e73ae64321c5c5f437dea1371defa7b059bd
AgitatedCrawdad <rocketpower444444@yahoo.com>
2017-07-07 16:47:18 -0500

    Added Plot2

--- Woods Analytical.py
+++ Woods Analytical.py
@@ -17,20 +17,29 @@ def bounds_x(i):
 def bounds_x2(i):
     return [0, np.inf]
 
+def f2(r2):
+    return r2**(lamb*beta-1)*np.exp(-(r2**2))
+
+def bounds_x21(i):
+    return [10E-6, r2[i]]
+def bounds_x22(i):
+    return [10E-6, 175]
+
 #Constants
 r = 1
-r2 = np.linspace(0.001,100,1000)
+r2 = np.logspace(-3,2,1000)
 exponent = 1
 k = 0.143*10**(-exponent)
-t1 = np.linspace(0.1,1000,1000)
+t1 = np.linspace(0.1,1000,1000) #np.linspace(0.1,1000,1000)
 t2 = 10
-Q = 0.5*10**(-exponent)
+Q = 0.5*10**(-exponent)*0.0033
 phi = 0.5
-T0 = 60
-T1 = 20
-lamb = 1
+T0 = 0
+T1 = 1
+lamb = 1.37
 beta = Q/k
 T = np.zeros(len(t1))
+theta = np.zeros(len(t1))
 
 omega = (Q/(2*(phi*k))**(0.5))
 
@@ -50,9 +59,11 @@ def Ex1():
 
 
 def Ex2():
-    for i in range(len(t1)):    
-        integrand, residual = integrate.nquad(f,[bounds_x(i)])
-        deltaT, residual2 = integrate.nquad(f,[bounds_x2(i)])
+    for i in range(len(r2)):    
+        integrand, residual = integrate.nquad(f2,[bounds_x21(i)])
+        deltaT, residual2 = integrate.nquad(f2,[bounds_x22(i)])
         T[i] = T0 + ((T1-T0)/(deltaT))*integrand
+        theta[i] = (T[i]-T0)/(T1-T0)
     plt.figure(1)
-    plt.semilogx(eta2,T)
+    plt.semilogx(r2,theta)
+    #plt.plot(r2,theta)
